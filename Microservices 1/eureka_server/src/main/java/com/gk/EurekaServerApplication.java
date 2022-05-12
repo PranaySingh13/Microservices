a8d0dd52ecb543705d48b113e4208d5d6f3827df
Pranay Singh <pranay.singh.1393@gmail.com>
2022-05-13 04:08:39 +0530

    eureka server micro service
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Microservices 1/eureka_server/src/main/java/com/gk/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.gk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//Annotation to activate Eureka Server related configuration. 
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
